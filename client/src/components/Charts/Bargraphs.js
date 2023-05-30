a8a177a83460f214e84f6d9639abc780eb4d7e9e
meghna <meghnabarn2001@gmail.com>
2023-05-30 18:02:21 +0530

    lib installed, graphs rectified

--- client/src/components/Charts/Bargraphs.js
+++ client/src/components/Charts/Bargraphs.js
@@ -6,16 +6,16 @@ import { CDBContainer } from 'cdbreact';
 import Chart from 'chart.js/auto';
 const Chart1 = () => {
   const [data] = useState({
-    labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+    labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
       {
-        label: 'My First dataset',
+        label: 'Income',
         backgroundColor: 'rgba(194, 116, 161, 0.5)',
         borderColor: 'rgb(194, 116, 161)',
         data: [65, 59, 90, 81, 56, 55, 40],
       },
       {
-        label: 'My Second dataset',
+        label: 'Expenses',
         backgroundColor: 'rgba(71, 225, 167, 0.5)',
         borderColor: 'rgb(71, 225, 167)',
         data: [28, 48, 40, 19, 96, 27, 100],
@@ -27,8 +27,8 @@ const Chart1 = () => {
     <CDBContainer>
       <h3 className="mt-5">Line chart</h3>
       <Line data={data} options={{ responsive: true }} />
-      <br/>
-      <br/><br/><br/><br/><br/><br/><br/>
+      <br />
+      <br /><br /><br /><br /><br /><br /><br />
     </CDBContainer>
   );
 };
