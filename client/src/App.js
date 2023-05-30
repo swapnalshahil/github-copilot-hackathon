4c01bbfdcc24ad0ca404d8d53e54212bc5b9c9bc
Swapnal Shahil <58564017+swapnalshahil@users.noreply.github.com>
2023-05-30 21:42:16 +0530

    Revert "Front-end changes"

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,11 @@
 
 import './App.css';
-import Welcomepage from './components/Welcomepage';
-import NavBarr from './components/Navbar/NavBarr';
-import Profile from './components/Profile/Profile';
-import Bargraphs from './components/Charts/Bargraphs';
+
 function App() {
   return (
-    // <div>
-    // {/*  */}
-    //</div>
-    // <welcomepage></welcomepage>
-    <>
-      <NavBarr />
-      <Profile />
-      <Bargraphs/>
-    </>
+    <div className="App">
+      <p>meowwwwwwwwwwwwwwww</p>
+    </div>
   );
 }
 
