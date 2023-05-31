27b0f5e1e3ed38776e95bca17f0e7e8cbdc0f050
Ayush Kumar Gupta <ayush@Ayushs-MacBook-Pro.local>
2023-05-31 17:04:34 +0530

    basic google auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <p>meowwwwwwwwwwwwwwww</p>
+      <Login />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <GoogleOAuthProvider clientId="961380917781-9k7lk1nfrmon547kc4k3bc8vmve98qtj.apps.googleusercontent.com">
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
