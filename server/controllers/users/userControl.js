57f081e5220b6899c0ab4dac623c865d3d52f408
Daksh Sharma <dakshsharma@Dakshs-MacBook-Pro.local>
2023-06-05 01:25:37 +0530

    Add month wise transaction API

--- server/controllers/users/userControl.js
+++ server/controllers/users/userControl.js
@@ -1,28 +1,65 @@
-const jwt = require('jsonwebtoken');
-const {createNewUser, findUserByEmail} = require('../../Repository/UserRepository')
+const jwt = require("jsonwebtoken");
+const { all } = require("../../app");
+const {
+  createNewUser,
+  findUserByEmail,
+  getUserTransactions
+} = require("../../Repository/UserRepository");
 // Register
 const loginUser = async (req, res) => {
-  const {email} = req?.body;
+  const { email } = req?.body;
   try {
     // logic to check if user exists then throw error
     // else do registeration
-    const jwtSecret = process.env.JWT_SECRET || 'secret'
-    const userExist = findUserByEmail(email)
-    if(!userExist) {
-      createNewUser({email});
+    const jwtSecret = process.env.JWT_SECRET || "secret";
+    const userExist = findUserByEmail(email);
+    if (!userExist) {
+      createNewUser({ email });
     }
-    const token = jwt.sign({
-     data: email,
-     exp: Math.floor(Date.now() / 1000) + (60 * 60), // expires in 1 hour
-    }, jwtSecret);
-    
-    res.status(200).json(token)
+    const token = jwt.sign(
+      {
+        data: email,
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 // expires in 1 hour
+      },
+      jwtSecret
+    );
 
+    res.status(200).json(token);
   } catch (error) {
     // throw error
-    console.log(error)
-    res.json(error)
+    console.log(error);
+    res.json(error);
   }
 };
-
-module.exports = {loginUser}
+const getLastYearDetails = async (req, res) => {
+  const { id } = req.body;
+  try {
+    let allTransactions = await getUserTransactions(id);
+    allTransactions = allTransactions.transactions;
+    const presentDate = new Date();
+    const currentYearTransactions = allTransactions.filter(
+      (currentTransaction) => {
+        const timeDifference =
+          (presentDate.getTime() -
+            currentTransaction.transactionDate.getTime()) /
+          (1000 * 3600 * 24);
+        return timeDifference < 365;
+      }
+    );
+    const monthWiseTransactionsDetail = allTransactions.reduce(
+      (reducer, currentTransaction) => {
+        const currentTransactionMonth =
+          currentTransaction.transactionDate.getMonth();
+        reducer[currentTransactionMonth] =
+          (reducer[currentTransactionMonth]
+            ? reducer[currentTransactionMonth]
+            : 0) + currentTransaction.amount;
+      },
+      {}
+    );
+    res.status(200).json({ monthWiseDetail: monthWiseTransactionsDetail });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { loginUser, getLastYearDetails };
--- server/routes/users/userRoutes.js
+++ server/routes/users/userRoutes.js
@@ -1,9 +1,18 @@
 //creating a route for the user
-const express = require('express');
-const { loginUser } = require('../../controllers/users/userControl');
+const express = require("express");
+const {
+  loginUser,
+  getLastYearDetails
+} = require("../../controllers/users/userControl");
+const { isAuthenticated } = require("../../middlewares/auth");
 
 const userRoute = express.Router();
 
-userRoute.post('/login', loginUser);
+userRoute.post("/login", loginUser);
+userRoute.get(
+  "/user/lastyeartransactions",
+  isAuthenticated,
+  getLastYearDetails
+);
 
 module.exports = userRoute;
