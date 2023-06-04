e62577160f04a999964d32f0be36bdd51f37e837
Ayush Kumar Gupta <ayushkguptaa@gmail.com>
2023-06-04 15:47:56 +0530

    Handle access denied error

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,14 +2,16 @@ const jwt = require('jsonwebtoken')
 const isAuthenticated = ( req, res, next ) => {
     const jwtSecret = process.env.JWT_SECRET;
     const token = req.header('Authorization')
-    const email = jwt.verify(token, jwtSecret);
-    if(email){
+    try{
+        const email = jwt.verify(token, jwtSecret);
         req.user = {
             email
         }
         next();
     }
-    else res.status(401).send({message: "Access Denied"});
+    catch (e) {
+        res.status(401).send({message: "Access Denied"});
+    }
 }
 
 module.exports = {
