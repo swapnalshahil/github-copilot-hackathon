29676168d3004fa9bcbefe12dd88eccea31cb20d
Swapnal <swapnalsahil@gmail.com>
2023-06-04 17:26:45 +0530

    dropdown on current balance && updated navbar with user image + alignment

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Pie } from "react-chartjs-2";
 
 const Dashboard = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [amountToAdd, setAmountToAdd] = useState("");
+  const [currentBalance, setCurrentBalance] = useState(500);
+
+  const handleDropdownToggle = () => {
+    setShowDropdown(!showDropdown);
+  };
+ const handleAddAmount = () => {
+   
+   if (!amountToAdd || isNaN(amountToAdd)) {
+     console.log("Invalid amount");
+     return;
+   }
+
+   const amount = parseFloat(amountToAdd);
+   
+   const newBalance = currentBalance + amount;
+   setCurrentBalance(newBalance);
+
+   
+   setAmountToAdd(0);
+   setShowDropdown(false);
+ };
+
+
+  const handleRemoveAmount = () => {
+  
+    const newBalance = currentBalance - parseFloat(amountToAdd);
+    setCurrentBalance(newBalance);
+
+    setAmountToAdd("");
+    setShowDropdown(false);
+  };
   // Sample data for the pie chart
   const pieChartData = {
     labels: ["Spent", "Remaining"],
@@ -17,7 +50,6 @@ const Dashboard = () => {
     elements: {
       arc: {
         borderWidth: 0,
-    
       },
     },
     responsive: true,
@@ -29,13 +61,70 @@ const Dashboard = () => {
     },
   };
 
-
   return (
     <div className="container flex flex-wrap justify-center space-x-4">
-      <div className="w-64 h-64 bg-white shadow-lg rounded-lg p-6 flex flex-col justify-between">
-        <div className="text-gray-500 text-sm mb-2">Current Balance</div>
-        <div className="text-2xl text-blue-500 font-semibold">INR 500</div>
+      <div className="relative">
+        <div className="w-64 h-64 bg-white shadow-lg rounded-lg p-6 flex flex-col justify-between">
+          <div className="text-gray-500 text-sm mb-2">Current Balance</div>
+          <div className="text-2xl text-blue-500 font-semibold">
+            INR {currentBalance}
+          </div>
+
+          <div className="absolute top-0 right-0">
+            <div className="relative inline-block text-left">
+              <div>
+                <button
+                  type="button"
+                  className="inline-flex justify-center items-center px-4 py-2 bg-gray-200 text-sm text-gray-700 font-medium rounded-md hover:bg-gray-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                  onClick={handleDropdownToggle}
+                >
+                  <svg
+                    className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                </button>
+              </div>
+              {showDropdown && (
+                <div className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                  <div
+                    className="py-1"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="options-menu"
+                  >
+                    <button
+                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                      role="menuitem"
+                      onClick={handleAddAmount}
+                    >
+                      Add Amount
+                    </button>
+                    <button
+                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                      role="menuitem"
+                      onClick={handleRemoveAmount}
+                    >
+                      Remove Amount
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
+
       <div className="w-64 h-64 bg-white shadow-lg rounded-lg p-6 flex flex-col justify-between">
         <div className="text-gray-500 text-sm mb-2">Amount Spent</div>
         <div className="text-2xl text-red-500 font-semibold">INR 300</div>
